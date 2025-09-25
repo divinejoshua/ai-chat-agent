2be2576c581189b9668975281c7ff14e723b7181
divinejoshua <divineuche399@gmail.com>
2025-09-25 10:11:26 +0100

    Added web search with URL context

--- web_search.py
+++ web_search.py
@@ -1,24 +1,23 @@
 from google import genai
 from decouple import config
-from google.genai import types
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch, UrlContext
 
 client = genai.Client(api_key=config("GEMINI_API_KEY"))
 
-grounding_tool = types.Tool(
-    google_search=types.GoogleSearch()
-)
+tools = [
+      {"url_context": {}},
+      {"google_search": {}}
+  ]
 
-config = types.GenerateContentConfig(
-    tools=[grounding_tool]
-)
-
-prompt = "Who won the euro 2024?"
+prompt = "What are the 5 ways to get a business Idea as found on this link https://medium.com/@qevoltlimited/5-ways-to-find-the-right-business-idea-6581dc6d9de6"
 
 
 response = client.models.generate_content_stream(
     model="gemini-2.5-flash", 
     contents=prompt,
-    config=config
+   config=GenerateContentConfig(
+        tools=tools,
+    )
     )
 
 for chunk in response:
